db2bd9b35af1131d5653aa54e14d642fdee8f24e
Ekwuno <obinnacodes@gmail.com>
2021-10-04 16:48:08 +0100

    adding all the stuff

---
+++ src/pages/posts/{WpPost.title}.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+export default function BlogPost({ data }) {
+  // const post = ;
+  // console.log(post);
+  return (
+    <div>
+      <div>
+        <h1>{data.WpPost.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: data.WpPost.excerpt }} />
+      </div>
+      
+    </div>
+  );
+}
+
+export const query = graphql`
+  query ($slug: String) {
+    wpPost(slug: { eq: $slug }) {
+      title
+      excerpt
+    }
+  }
+`;
